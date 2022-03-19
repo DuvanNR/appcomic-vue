dbec1cf3b16133ba5ad51f939f466d0ad8286da8
Duvan <dunavarror@gmail.com>
2022-03-19 12:25:28 -0500

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# abort on errorss
 set -e
 
 # build
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:DuvanNR/appcomic-vue.git main:gh-pages
+git push -f git@github.com:DuvanNR/appcomic-vue.git master:gh-pages
 
 cd -
